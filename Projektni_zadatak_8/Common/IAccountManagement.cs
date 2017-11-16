fdfee1b312b1220e4ca931a543e701ced3110dbc
UrosNikolic95 <4.3.1995.urosnikolic@gmail.com>
2017-11-16 12:21:30 +0100

    Added interfaces

---
+++ Projektni_zadatak_8/Common/IAccountManagement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+
+namespace Common
+{
+    [ServiceContract]
+    public interface IAccountManagement
+    {
+        [OperationContract]
+        bool CreateAccount(string username);
+
+        [OperationContract]
+        bool DeleteAccount(string username);
+
+        [OperationContract]
+        bool LockAccount(string username);
+
+        [OperationContract]
+        bool EnableAccount(string username);
+
+        [OperationContract]
+        bool DisableAccount(string username);
+
+    }
+}
