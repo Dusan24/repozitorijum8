d2db25adb2a1eadaa783e16f9e121542d238d760
UrosNikolic95 <4.3.1995.urosnikolic@gmail.com>
2017-11-20 14:07:35 +0100

    my commit

--- Projektni_zadatak_8/ClientAdmins/Program.cs
+++ Projektni_zadatak_8/ClientAdmins/Program.cs
@@ -16,9 +16,16 @@ namespace ClientAdmins
             NetTcpBinding tb = new NetTcpBinding();
 
 
+
             using (ProxyClientAdmins p = new ProxyClientAdmins(tb, ServiceAddresses.CredentialServiceAddress))
             {
+<<<<<<< HEAD
                 p.CreateAccount("user1","123");
+=======
+
+         
+
+>>>>>>> 634f1cdeb83fd7a18c21cdd9bac4247a4b2ba2ff
                 Console.ReadKey();
             }
 
