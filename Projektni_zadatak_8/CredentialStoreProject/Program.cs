8fff9247b1c96805210257c52570c1a8bc7b3d6a
UrosNikolic95 <4.3.1995.urosnikolic@gmail.com>
2017-11-20 12:46:40 +0100

    A

--- Projektni_zadatak_8/CredentialStoreProject/Program.cs
+++ Projektni_zadatak_8/CredentialStoreProject/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +13,20 @@ namespace CredentialStoreProject
         static void Main(string[] args)
         {
 
+            NetTcpBinding binding = new NetTcpBinding();
 
+            
+
+            ServiceHost host = new ServiceHost(typeof(CredentialService));
+            host.AddServiceEndpoint(typeof(IAccountManagement), binding, ServiceAddresses.CredentialServiceAddress);
+
+
+            host.Open();
+
+            Console.ReadLine();
+
+
+            host.Close();
 
         }
     }
