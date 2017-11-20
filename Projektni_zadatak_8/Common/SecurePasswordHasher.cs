2a878e27bf83f968bd0cab8abb7fe8acd20c1882
UrosNikolic95 <4.3.1995.urosnikolic@gmail.com>
2017-11-20 16:40:28 +0100

    Fixed Hashing

--- Projektni_zadatak_8/Common/SecurePasswordHasher.cs
+++ Projektni_zadatak_8/Common/SecurePasswordHasher.cs
@@ -9,95 +9,46 @@ namespace Common
 {
     public static class SecurePasswordHasher
     {
-        /// <summary>
-        /// Size of salt
-        /// </summary>
+
         private const int SaltSize = 16;
 
-        /// <summary>
-        /// Size of hash
-        /// </summary>
-        private const int HashSize = 20;
-
-        /// <summary>
-        /// Creates a hash from a password
-        /// </summary>
-        /// <param name="password">the password</param>
-        /// <param name="iterations">number of iterations</param>
-        /// <returns>the hash</returns>
         public static string Hash(string password)
         {
-            HashAlgorithm hash;
-            //create salt
-            byte[] salt;
-            new RNGCryptoServiceProvider().GetBytes(salt = new byte[SaltSize]);
 
-            //create hash
-            hash = new SHA256Managed();
-            var hashVal = ASCIIEncoding.UTF8.GetBytes(password);
+            HashAlgorithm hash_alg = new SHA256Managed();
+
+            byte[] byte_salt;
+            new RNGCryptoServiceProvider().GetBytes(byte_salt = new byte[SaltSize]);
+            string salt = Convert.ToBase64String(byte_salt);
 
 
-            //combine salt and hash
-            var hashBytes = new byte[SaltSize + HashSize];
-            Array.Copy(salt, 0, hashBytes, 0, SaltSize);
-            if (hashVal.Length < HashSize)
-            {
-                Array.Copy(hashVal, 0, hashBytes, SaltSize, hashVal.Length);
-            }
-            else
-            {
-                Array.Copy(hashVal, 0, hashBytes, SaltSize,HashSize);
-            }
+            string for_hashing = password + salt;
+            string hashed_password = Convert.ToBase64String(hash_alg.ComputeHash(ASCIIEncoding.UTF8.GetBytes(for_hashing)));
+            string salted_hashed_password = hashed_password + salt;
+
+            return salted_hashed_password;
 
 
-
-            byte[] bte= hash.ComputeHash(hashBytes);
-
-            //convert to base64
-        
-            var retVal = String.Concat(Convert.ToBase64String(bte), Convert.ToBase64String(salt));
-            //format hash with extra information
-            return retVal;
         }
 
-        /// <summary>
-        /// verify a password against a hash
-        /// </summary>
-        /// <param name="password">the password</param>
-        /// <param name="hashedPassword">the hash</param>
-        /// <returns>could be verified?</returns>
+
         public static bool Verify(string password, string hashedPassword)
         {
 
+            HashAlgorithm hash_alg = new SHA256Managed();
 
-            //extract iteration and Base64 string
-            byte[] pass = ASCIIEncoding.UTF8.GetBytes(password);
-            byte[] hashpass = ASCIIEncoding.UTF8.GetBytes(hashedPassword);
+            string salt = hashedPassword.Substring(hashedPassword.Length - SaltSize - 8);
+            string forhashing = password + salt;
 
-            byte[] hash = new byte[HashSize];
-            byte[] salt = new byte[SaltSize];
+            string hashedpasword1 = Convert.ToBase64String(hash_alg.ComputeHash(ASCIIEncoding.UTF8.GetBytes(forhashing)));
 
-            Array.Copy(hashpass, hash, HashSize);
-            Array.Copy(hashpass, HashSize, salt, 0, SaltSize);
+            string hashedpasword2 = hashedPassword.Substring(0, hashedPassword.Length - SaltSize - 8);
 
-            var hashBytes = new byte[SaltSize + HashSize];
-            Array.Copy(salt, 0, hashBytes, 0, SaltSize);
-            if (pass.Length < HashSize)
-            {
-                Array.Copy(pass, 0, hashBytes, SaltSize, pass.Length);
-            }
-            else
-            {
-                Array.Copy(pass, 0, hashBytes, SaltSize, HashSize);
-            }
 
-            HashAlgorithm hashalg;
-            hashalg = new SHA256Managed();
-            byte[] newhash= hashalg.ComputeHash(hashBytes);
+            return hashedpasword1 == hashedpasword2;
 
-            return (hashpass.SequenceEqual(newhash));
-
-            
         }
+
+
     }
 }
